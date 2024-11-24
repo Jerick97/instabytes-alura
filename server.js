cc7e266589d2bde7f2090d701229b4dc78e28677
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-11-24 18:41:27 -0500

    feat: :sparkles: Configura servidor con Node.js, crea endpoints para posts y posts/:id, y conecta base de datos MongoDB

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 import express from "express";
 
 const posts = [
-    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+	{
+		id: 1,
+		descricao: "Uma foto teste",
+		imagem: "https://placecats.com/millie/300/150",
+	},
+	{
+		id: 2,
+		descricao: "Gato fazendo yoga",
+		imagem: "https://placecats.com/neo/300/200",
+	},
+	{
+		id: 3,
+		descricao: "Gato fazendo panqueca",
+		imagem: "https://placecats.com/bella/300/200",
+	},
 ];
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+	console.log("Servidor escuchando en el puerto 3000");
 });
 
 app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
+	res.status(200).json(posts);
 });
 
 function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
+	return posts.findIndex((post) => {
+		return post.id === Number(id);
+	});
 }
 
 app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
+	const index = buscarPostPorID(req.params.id);
+	res.status(200).json(posts[index]);
 });
